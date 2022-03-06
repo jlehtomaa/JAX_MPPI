3c9dc919f0ed14699c59d9d0eaee661c46e47bef
jlehtomaa <jere.lehtomaa@gmail.com>
2022-03-06 19:10:28 +0100

    cleanup main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#%%
 import gym
 import jax.numpy as jnp
 from src.utils import read_config
 from src.mppi import MPPI
-# %%
-ID = "pendulum"
-CFG_PATH = "./config/" + ID + ".yaml"
+
+
+CFG_PATH = "./config/pendulum.yaml"
 config = read_config(CFG_PATH)
 
 mppi = MPPI(config)
@@ -13,12 +12,10 @@ env = gym.make("Pendulum-v0")
 
 mppi.reset()
 obs, done, rew, n_steps = env.reset(), False, 0.0, 0
-obs = obs.reshape((-1,1))
 
 while not done:
-    act = mppi.get_action(obs)
+    act = mppi.get_action(obs.reshape((-1,1)))
     obs, r, done, _ = env.step(act)
-    obs = obs.reshape((-1,1))
     rew += r
     n_steps += 1
-    #env.render()
+    env.render()
